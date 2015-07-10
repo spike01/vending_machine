88d8cb780fe860c47307920e28e1b1215c87ade3
Spike Nox <spike01@gmail.com>
2015-07-10 08:18:01 +0100

    RED - Add test for inserting nickel
    Using language that's as close to real life, I write a test that tries to
    model this sentence in the feature: "When a valid coin is inserted the amount
    of the coin will be added to the current amount and the display will be
    updated". Note two things: 1. The #message method returns a string, even when
    displaying a monetary amount. 2. At this point I don't care about the
    implementation of the coin, and use a double in its place.

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -10,5 +10,13 @@ describe VendingMachine do
         expect(vending_machine.message).to eq("INSERT COIN")
       end
     end
+
+    context "when inserting valid coins" do
+      it "accepts nickels" do
+        nickel = double(:coin)
+        vending_machine.insert(nickel)
+        expect(vending_machine.message).to eq("0.05")
+      end
+    end
   end
 end
