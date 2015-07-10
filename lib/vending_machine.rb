08199be9b8e52e85f30e81475a75cb2e1de5149f
Spike Nox <spike01@gmail.com>
2015-07-10 09:41:33 +0100

    GREEN - Accept a quarter
    My tests pass, but having seen that adding a new kind of coin requires
    me to modify an existing method, I'm going to clean up a little bit.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -11,7 +11,8 @@ class VendingMachine
   def insert(coin)
     weights_to_values = {
       5 => 0.05,
-      2.26 => 0.10
+      2.26 => 0.10,
+      5.67 => 0.25
     }
     @display = "%.2f" % weights_to_values[coin.weight]
   end
